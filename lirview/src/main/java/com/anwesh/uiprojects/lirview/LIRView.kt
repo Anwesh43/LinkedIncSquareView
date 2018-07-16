338b95e7d236336cac2efe52d717f31df5a8126b
Anwesh43 <anweshthecool0@gmail.com>
2018-07-16 10:34:41 +0530

    importing modules and adding nodes number

--- lirview/src/main/java/com/anwesh/uiprojects/lirview/LIRView.kt
+++ lirview/src/main/java/com/anwesh/uiprojects/lirview/LIRView.kt
@@ -3,3 +3,12 @@ package com.anwesh.uiprojects.lirview
 /**
  * Created by anweshmishra on 16/07/18.
  */
+
+import android.view.View
+import android.content.Context
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+
+val nodes : Int = 5
