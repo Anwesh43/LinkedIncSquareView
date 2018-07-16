ee18d7d07861630c13fd9c8d41cab08d80b1eea1
Anwesh43 <anweshthecool0@gmail.com>
2018-07-16 10:41:20 +0530

    adding extension methods

--- lirview/src/main/java/com/anwesh/uiprojects/lirview/LIRView.kt
+++ lirview/src/main/java/com/anwesh/uiprojects/lirview/LIRView.kt
@@ -13,6 +13,28 @@ import android.graphics.RectF
 
 val nodes : Int = 5
 
+fun Canvas.drawSquareNode(i : Int, paint : Paint) {
+    val gap = (Math.min(width, height).toFloat()) / nodes
+    val size : Float = gap * (i + 1)
+    drawAtMid {
+        drawRoundedSquare(0f, 0f, gap, paint)
+    }
+}
+
+fun Canvas.drawAtMid(cb : () -> Unit) {
+    save()
+    translate(width.toFloat()/2, height.toFloat()/2)
+    cb()
+    restore()
+}
+
+fun Canvas.drawRoundedSquare(x : Float, y : Float, a : Float, paint : Paint) {
+    save()
+    translate(x, y)
+    drawRoundRect(RectF(-a/2, -a/2, a/2, a/2), a/5, a/5, paint)
+    restore()
+}
+
 class LIRView(ctx : Context) : View(ctx) {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
